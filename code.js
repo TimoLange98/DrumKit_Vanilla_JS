266da657e60f1f2e9c8bb721fea00b32e5251187
TimoL <timo.lange98@aol.de>
2020-09-10 01:30:55 +0200

    Ready

--- code.js
+++ code.js
@@ -1,7 +1,33 @@
-const soundOnClick = (id) => {
-    var element = document.getElementById(id);
-    element.classList.add('playing');
-    setTimeout(() => {
-        element.classList.remove('playing')
-    }, 500);
+const soundOnClick = async (id) => {
+    await bump(id);
+    await playSound(id);
 }
+
+const bump = (id) => {
+    var element = document.getElementById(id);
+    return new Promise(resolve => {
+        element.classList.add('playing');
+        setTimeout(() => {
+            element.classList.remove('playing')
+        }, 100);
+        resolve();
+    })
+}
+
+const playSound = (id) => {
+    return new Promise(resolve => {
+        var audioElement = document.querySelector(`audio[data-key="${id}"]`);
+        audioElement.currentTime = 0;
+        audioElement.play();
+        resolve();
+    })
+}
+
+const availableKeys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+
+document.addEventListener('keydown', (e) => {
+    if (availableKeys.includes(e.key.toLowerCase())) {
+        console.log('it works');
+        soundOnClick(e.key.toLowerCase());
+    }
+})
